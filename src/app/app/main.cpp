21ca4af7cae624d25be5796bad307df42df862bd
Ghazan <ghazan54>
2023-09-05 12:43:26 +0700

    Fix init variables

--- src/app/app/main.cpp
+++ src/app/app/main.cpp
@@ -8,15 +8,15 @@ int main(int argc, char *argv[]) {
     try {
         CLI::App app("fts sum");
 
-        int firstCount;
-        app.add_option("--first", firstCount, "First count")->required();
+        int first_count = 0;
+        app.add_option("--first", first_count, "First count")->required();
 
-        int secondCount;
-        app.add_option("--second", secondCount, "Second count")->required();
+        int second_count = 0;
+        app.add_option("--second", second_count, "Second count")->required();
 
         CLI11_PARSE(app, argc, argv);
 
-        std::cout << fts::sum(firstCount, secondCount) << '\n';
+        std::cout << fts::sum(first_count, second_count) << '\n';
 
     } catch (const std::exception &e) {
         std::cerr << e.what() << '\n';
